ea6c5b26541f52d6e8db01494162b05feb3b7fa3
bartekcostam <bartekoffice1@gmail.com>
2018-05-29 23:23:47 +0100

    .

--- scripts/vue.js
+++ scripts/vue.js
@@ -1,14 +1,6 @@
 Vue.component("addStopWatch", {
 
-template:'
-
-<div id="section" class="text-center">
-    <h1><time>00:00:00</time></h1>
-<button id="start" class="btn btn-outline-success">start</button>
-<button id="stop" class="btn btn-outline-primary">stop</button>
-<a id="clear" href="#myModal" class="btn btn-outline-danger" data-toggle="modal" data-target="#myModal">clear</a>
-<h1 class="title">Tytul zadania</h1>
-</div>',
+template:''
 
 
 
