f66397bb4cf60f30d1ed405eabdb94b3e9db81d7
bartekcostam <bartekoffice1@gmail.com>
2018-05-30 23:58:09 +0100

    clone element finally working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -52,10 +52,18 @@ deleteTime.onclick = function() {
 seconds = 0; minutes = 0; hours = 0;
 }
 
-plus.onclick = function(){
 
-    var addNote = write.document("<h1>costam</h1>");
 
-    
+    var i = 0;
+var original = document.getElementById('duplicater');
+
+function duplicate() {
+    var clone = original.cloneNode(true); // "deep" clone
+    clone.id = "duplicater" + ++i;
+    // or clone.id = ""; if the divs don't need an ID
+    original.parentNode.appendChild(clone);
 }
+    
+
+
 
