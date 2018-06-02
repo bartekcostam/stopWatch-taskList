e299cee5317fb06f6885d1743d1b4f2ef6d94656
bartekcostam <bartekoffice1@gmail.com>
2018-06-02 23:45:15 +0100

    ...

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -58,7 +58,7 @@ seconds = 0; minutes = 0; hours = 0;
 var original = document.getElementById('duplicater');
 
 function duplicate() {
-    var clone = original.cloneNode(true); // "deep" clone
+    var clone = original.cloneNode(true); // "deep" clone so I can try to do it in diffrent way 
     clone.id = "duplicater" + ++i;
     // or clone.id = ""; if the divs don't need an ID
     original.parentNode.appendChild(clone);
