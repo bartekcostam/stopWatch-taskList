3bc22f56bc00020fe2419c3d857d6c4924e7c21d
bartekcostam <bartekoffice1@gmail.com>
2018-06-04 23:50:25 +0100

    h1 title to text area to innerHTML

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,75 +1,63 @@
-
-var h1 = document.getElementsByTagName('h1')[0],
-start = document.getElementById('start'),
-stop = document.getElementById('stop'),
-clear = document.getElementById('clear'),
-plus = document.getElementById('plus'),
-seconds = 0, minutes = 0, hours = 0,
-t,
-
-
-
-addNote,
-deleteNote,
-openNote;
-
-
+var h1 = document.getElementsByTagName("h1")[0],
+  start = document.getElementById("start"),
+  stop = document.getElementById("stop"),
+  clear = document.getElementById("clear"),
+  plus = document.getElementById("plus"),
+  seconds = 0,
+  minutes = 0,
+  hours = 0,
+  t,
+  addNote,
+  deleteNote,
+  openNote;
 
 function add() {
-seconds++;
-if (seconds >= 60) {
+  seconds++;
+  if (seconds >= 60) {
     seconds = 0;
     minutes++;
     if (minutes >= 60) {
-        minutes = 0;
-        hours++;
+      minutes = 0;
+      hours++;
     }
-}
+  }
 
+  h1.textContent =
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+    ":" +
+    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+    ":" +
+    (seconds > 9 ? seconds : "0" + seconds);
 
-
-h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-
-timer();
+  timer();
 }
 function timer() {
-t = setTimeout(add, 1000);
-
+  t = setTimeout(add, 1000);
 }
 
-
-
-
 /* Start button */
-
 
 start.onclick = timer;
 
 /* Stop button */
 stop.onclick = function() {
-clearTimeout(t);
-}
+  clearTimeout(t);
+};
 
 /* Clear button */
 
+deleteTime.onclick = function() {
+  h1.textContent = "00:00:00";
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+};
 
-deleteTime.onclick = function() { 
-    h1.textContent = "00:00:00";
-seconds = 0; minutes = 0; hours = 0;
-}
-
-
-
-    
-var original = document.getElementById('duplicater');
+var original = document.getElementById("duplicater");
 
 function duplicate() {
-    var clone = original.cloneNode(true); // "deep" clone so I can try to do it in diffrent way 
-    clone.id = "duplicater";
-    // or clone.id = ""; if the divs don't need an ID
-    original.parentNode.appendChild(clone);
+  var clone = original.cloneNode(true); // "deep" clone so I can try to do it in diffrent way
+  clone.id = "duplicater";
+  // or clone.id = ""; if the divs don't need an ID
+  original.parentNode.appendChild(clone);
 }
-    
-
-
-
