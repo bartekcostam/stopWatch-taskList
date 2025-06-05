656c52d11d47e211dd51884380a8334d12bdfc63
Bartlomiej Wydmuch <bartekoffice1@gmail.com>
2025-06-05 16:32:31 +0200

    Fix typo in cache memory comment

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -89,6 +89,6 @@ function duplicate() {
 
   //in this step I should run onload method too, to reload clone DOM elements
 
-  //I can try to save some data to ceche memory
+  // I can try to save some data to cache memory
 }
 
